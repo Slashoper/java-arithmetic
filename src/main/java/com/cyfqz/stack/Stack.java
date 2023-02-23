4a780d24658eb5a11351b1787802ce3f0ae25305
李庆安 <liqan@szlanyou.com>
2023-02-23 19:12:55 +0800

    modify directory name

--- src/main/java/com/cyfqz/stack/Stack.java
+++ src/main/java/com/cyfqz/stack/Stack.java
@@ -1,15 +1,28 @@
 package com.cyfqz.stack;
 
+import java.util.Random;
+
 /**
  *
- * 基于数组实现栈,index反应,index指向第一个节点
+ * 基于数组实现栈,index反应,index指向第一个节点 , 栈很简单
  */
 public class Stack {
     int[] data;
     int index;
+
+    int capacity;
     public Stack(int capacity){
+        this.capacity = capacity;
         data = new int[capacity];
         index =-1;
+    }
+
+    public boolean isEmpty(){
+        return index == -1;
+    }
+
+    public boolean isFull(){
+        return index == capacity-1;
     }
 
     public int pop(){
@@ -26,18 +39,25 @@ public class Stack {
         this.data[index] = data;
     }
 
+    public int peek(){
+        return index > 0 ? data[index] : -1;
+    }
+
     public static void main(String[] args) {
-        Stack stack = new Stack(100);
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        stack.push(5);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        int len = 20;
+        Random random = new Random();
+        Stack stack = new Stack(len);
+        System.out.println("入栈顺序为：");
+        for(int i=0; i<len; i++){
+            int e = random.nextInt(len);
+            System.out.print(e+"-->");
+            stack.push(e);
+        }
+        System.out.println("\n出栈顺序为: ");
+        while (!stack.isEmpty()){
+            int e = stack.pop();
+            System.out.print(e+"-->");
+        }
     }
 
 }
